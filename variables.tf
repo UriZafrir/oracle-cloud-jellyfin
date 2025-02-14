067f65997b3d148e23129d83a763d8cb0fa79675
urizaf <urizaf@gmail.com>
2025-02-14 17:45:18 +0200

    a

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "compartment_ocid" {
+  type        = string
+  description = "OCID of the compartment where resources will be created"
+}
+
+variable "instance_display_name" {
+  type        = string
+  description = "Display name for the compute instance"
+}
+
+variable "source_ocid" {
+  type        = string
+  description = "OCID of the source image or boot volume"
+}
+
+variable "subnet_ocids" {
+  type        = list(string)
+  description = "List of subnet OCIDs where the instance will be created"
+}
+
+variable "public_ip" {
+  type        = string
+  description = "Whether to create a public IP. Valid values: NONE, RESERVED or EPHEMERAL"
+}
+
+variable "ssh_public_keys" {
+  type        = string
+  description = "SSH public key for instance access"
+}
+
+variable "shape" {
+  type        = string
+  description = "Shape of the instance"
+}
+
+variable "instance_state" {
+  type        = string
+  description = "Desired state of the instance. Valid values: RUNNING or STOPPED"
+}
+
+variable "boot_volume_backup_policy" {
+  type        = string
+  description = "Backup policy for the boot volume. Valid values: disabled, gold, silver or bronze"
+}
