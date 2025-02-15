3b6161dbad0fdbf0e05c96eebc3561e4a9194c30
urizaf <urizaf@gmail.com>
2025-02-15 21:41:44 +0200

    a

--- provider.tf
+++ provider.tf
@@ -9,9 +9,9 @@ terraform {
 }
 
 provider "oci" {
-  tenancy_ocid = var.tenancy_id#"ocid1.tenancy.oc1..aaaaaaaa66wkuhn3lioj44arbaokoeukfjlnt2njqghj52faoaganmsfl35a"
-  user_ocid = var.user_ocid #"ocid1.user.oc1..aaaaaaaablgybdsk4lpod3ntahqrpfhdb3dvwlxeu4i4e4fw6lforfysrw6a" 
+  tenancy_ocid     = var.tenancy_id #"ocid1.tenancy.oc1..aaaaaaaa66wkuhn3lioj44arbaokoeukfjlnt2njqghj52faoaganmsfl35a"
+  user_ocid        = var.user_ocid  #"ocid1.user.oc1..aaaaaaaablgybdsk4lpod3ntahqrpfhdb3dvwlxeu4i4e4fw6lforfysrw6a" 
   private_key_path = "C:/Users/uriza/.oci/rsa-key.pem"
-  fingerprint = "65:83:f6:25:5c:4d:6c:ab:12:d9:9b:74:ed:4c:7d:fd"
-  region = var.region#"eu-frankfurt-1"#"il-jerusalem-1"
+  fingerprint      = "65:83:f6:25:5c:4d:6c:ab:12:d9:9b:74:ed:4c:7d:fd"
+  region           = var.region #"eu-frankfurt-1"#"il-jerusalem-1"
 }
